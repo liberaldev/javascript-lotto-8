c4ae53c09af6d74f8821839dc1335e8d5f9a652e
user <ssharp0614@gmail.com>
2025-11-02 23:45:21 +0900

    feat(Lotto): 개별 로또 유효성 검사 강화

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,14 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+
+    if (numbers.some((number) => number < 1 || number > 45)) {
+      throw new Error('[ERROR] 각 로또 번호는 1부터 45까지여야 합니다.');
+    }
+
+    if (new Set(numbers).size !== 6) {
+      throw new Error('[ERROR] 로또 번호는 중복된 숫자가 있으면 안됩니다.');
+    }
   }
 
   getNumbers() {
