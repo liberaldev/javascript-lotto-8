835b01e65961731aecee1f311a7481afb8fc07cd
user <ssharp0614@gmail.com>
2025-11-02 18:41:59 +0900

    feat(App): 입력한 구입 금액 유효성 검사 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,8 +3,25 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 class App {
   #amount;
 
+  #validateAmount() {
+    if (Number.isNaN(this.#amount)) {
+      throw new Error('[ERROR] 숫자를 입력하십시오');
+    }
+    if (this.#amount % 1000 !== 0) {
+      throw new Error('[ERROR] 1000원으로 나누어떨어지지 않습니다. 1000원 단위로 입력하십시오');
+    }
+  }
+
+
+
   async run() {
-    this.#amount = Number(await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n'));
+    try {
+      this.#amount = Number(await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n'));
+      this.#validateAmount();
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+      this.#amount = Number(await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n'));
+    }
   }
 }
 
