b51c99ce73b31a092151c1c187582181bf28d316
user <ssharp0614@gmail.com>
2025-11-03 22:56:24 +0900

    refactor(LottoRanking): 외부에서 접근할 일이 없는 필드를 private 필드로 전환

--- src/LottoRanking.js
+++ src/LottoRanking.js
@@ -1,13 +1,13 @@
 class LottoRanking {
-  static FIRST = new LottoRanking('1st', 2000000000, 6, false, '6개 일치');
+  static #FIRST = new LottoRanking('1st', 2000000000, 6, false, '6개 일치');
 
-  static SECOND = new LottoRanking('2nd', 30000000, 5, true, '5개 일치, 보너스 볼 일치');
+  static #SECOND = new LottoRanking('2nd', 30000000, 5, true, '5개 일치, 보너스 볼 일치');
 
-  static THIRD = new LottoRanking('3rd', 1500000, 5, false, '5개 일치');
+  static #THIRD = new LottoRanking('3rd', 1500000, 5, false, '5개 일치');
 
-  static FOURTH = new LottoRanking('4th', 50000, 4, false, '4개 일치');
+  static #FOURTH = new LottoRanking('4th', 50000, 4, false, '4개 일치');
 
-  static FIFTH = new LottoRanking('5th', 5000, 3, false, '3개 일치');
+  static #FIFTH = new LottoRanking('5th', 5000, 3, false, '3개 일치');
 
   constructor(key, prize, matchCount, hasBonusNum, description) {
     this.key = key;
@@ -18,7 +18,7 @@ class LottoRanking {
   }
 
   static values() {
-    return [this.FIRST, this.SECOND, this.THIRD, this.FOURTH, this.FIFTH];
+    return [this.#FIRST, this.#SECOND, this.#THIRD, this.#FOURTH, this.#FIFTH];
   }
 
   static findRanking(matchCount, hasBonusNum) {
