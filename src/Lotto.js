5fc4d7f2343ba2e436e024ab0b3f9efaf009671d
user <ssharp0614@gmail.com>
2025-11-03 20:10:17 +0900

    refactor(Lotto): 유효성 검사 메소드에서 하드코딩된 부분 상수화

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,15 +7,18 @@ class Lotto {
   }
 
   static #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    const LOTTO_SIZE = 6;
+    const MIN_NUMBER = 1; const MAX_NUMBER = 45;
+
+    if (numbers.length !== LOTTO_SIZE) {
+      throw new Error(`[ERROR] 로또 번호는 ${LOTTO_SIZE}개여야 합니다.`);
     }
 
-    if (numbers.some((number) => number < 1 || number > 45)) {
-      throw new Error('[ERROR] 각 로또 번호는 1부터 45까지여야 합니다.');
+    if (numbers.some((number) => number < MIN_NUMBER || number > MAX_NUMBER)) {
+      throw new Error(`[ERROR] 각 로또 번호는 ${MIN_NUMBER}부터 ${MAX_NUMBER}까지여야 합니다.`);
     }
 
-    if (new Set(numbers).size !== 6) {
+    if (new Set(numbers).size !== LOTTO_SIZE) {
       throw new Error('[ERROR] 로또 번호는 중복된 숫자가 있으면 안됩니다.');
     }
   }
