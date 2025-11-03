6b9291dcdc2b31cd66447b8f601f410f805ef89b
user <ssharp0614@gmail.com>
2025-11-03 18:09:40 +0900

    test(LottoTicketsTest): add와 getTickets 메소드에 대한 테스트 추가

---
+++ __tests__/LottoTicketsTest.js
@@ -0,0 +1,11 @@
+import LottoTickets from '../src/LottoTickets.js';
+import Lotto from '../src/Lotto.js';
+
+describe('로토 티켓스 클래스 테스트', () => {
+  test('add/getTickets 메소드 테스트', () => {
+    const lottoTickets = new LottoTickets();
+    const lottoTicket = new Lotto([1, 2, 3, 4, 5, 6]);
+    lottoTickets.add(lottoTicket);
+    expect(lottoTickets.getTickets()).toEqual([lottoTicket]);
+  });
+});
